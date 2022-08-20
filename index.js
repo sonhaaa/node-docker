49c528885d54f575fab67d8d98421ff1cd536f92
sonhaaa <nguyensonha.hanz@gmail.com>
2022-08-20 14:03:25 +0700

    change dummy

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.use(
 app.use(express.json())
 
 app.get('/api/v1', (req, res) => {
-  res.send('<h2>Hello 🤞 !asdsdasdaadzzzz</h2>')
+  res.send('<h2>Hello 🤞!</h2>')
   console.log('ran')
 })
 
